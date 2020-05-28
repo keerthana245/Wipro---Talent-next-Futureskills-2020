780f812b651788bdaf29349999258b19166b2dbb
keerthana245 <58820343+keerthana245@users.noreply.github.com>
2020-05-28 19:16:25 +0530

    Create commandline.java

---
+++ Day3 28.03.2020/commandline.java
@@ -0,0 +1,27 @@
+public class CommandLine
+{
+public static void main(String[] args)
+{
+System.out.println("welcome"+" "+args[0]);
+}
+}
+
+public class Commandline2
+{
+public static void main(String[] args)
+{
+System.out.println(args[0]+" "+"Technologies"+" "+args[1]);
+}
+}
+
+
+class Commandline1
+{
+public static void main(String[] args)
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+int sum=a+b;
+System.out.println(sum);
+}
+}
